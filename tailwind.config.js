8fb675bc2a4cb2ace731757929e90665935f65f0
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-12 23:52:42 +0200

    Added a favicon. Added colors. Added default font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        strongCyan: "hsl(172, 67%, 45%)",
+        veryDarkCyan: "hsl(183, 100%, 15%)",
+        darkGrayishCyan: "hsl(186, 14%, 43%)",
+        grayishCyan: "hsl(184, 14%, 56%)",
+        lightGrayishCyan: "hsl(185, 41%, 84%)",
+        veryLightGrayishCyan: "hsl(189, 41%, 97%)",
+      },
+    },
   },
   plugins: [],
 };
